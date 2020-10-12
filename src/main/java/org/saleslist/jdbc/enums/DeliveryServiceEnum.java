5cf66da69bd5989f70c8b5461bed03fa1946ea00
sergias <sergias@ukr.net>
2020-10-12 20:22:32 +0300

    Added a couple new values.

--- src/main/java/org/saleslist/jdbc/enums/DeliveryServiceEnum.java
+++ src/main/java/org/saleslist/jdbc/enums/DeliveryServiceEnum.java
@@ -2,5 +2,6 @@ package org.saleslist.jdbc.enums;
 
 public enum DeliveryServiceEnum {
 	NOVA_POST,
+	UKR_POSHTA,
 	COLLECTION_IN_PERSON
 }
--- src/main/java/org/saleslist/jdbc/enums/MarketPlaceEnum.java
+++ src/main/java/org/saleslist/jdbc/enums/MarketPlaceEnum.java
@@ -2,5 +2,6 @@ package org.saleslist.jdbc.enums;
 
 public enum MarketPlaceEnum {
 	OLX,
+	FACEBOOK,
 	OTHER
 }
