b6c769e4ebcc94a3af5701e281940ad156135f4d
sergias <sergias@ukr.net>
2020-12-18 19:54:50 +0200

    Changed sql script - turned off sequences changes

--- src/main/resources/db/copyFromBackup.sql
+++ src/main/resources/db/copyFromBackup.sql
@@ -2,9 +2,9 @@ DELETE FROM sales_db.public.user_roles;
 DELETE FROM sales_db.public.users;
 DELETE FROM sales_db.public.payouts;
 DELETE FROM sales_db.public.products;
-ALTER SEQUENCE sales_db.public.user_seq RESTART WITH 104;
-ALTER SEQUENCE sales_db.public.payouts_seq RESTART WITH 1500;
-ALTER SEQUENCE sales_db.public.products_seq RESTART WITH 250;
+-- ALTER SEQUENCE sales_db.public.user_seq RESTART WITH 104;
+-- ALTER SEQUENCE sales_db.public.payouts_seq RESTART WITH 1500;
+-- ALTER SEQUENCE sales_db.public.products_seq RESTART WITH 250;
 
 INSERT INTO sales_db.public.users SELECT * FROM copy.users_bu;
 INSERT INTO sales_db.public.user_roles SELECT * FROM copy.user_roles_bu;
